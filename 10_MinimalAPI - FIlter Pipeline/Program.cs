f7cf6355f49866d3b4d50bc7a0b0630f71c9e8d4
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-06-18 12:05:43 +1000

    added open API

--- 10_MinimalAPI - FIlter Pipeline/Program.cs
+++ 10_MinimalAPI - FIlter Pipeline/Program.cs
@@ -5,7 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IEmployeesRepository, EmployeesRepository>();
 
+builder.Services.AddOpenApi();      // Add OPEN API to services collection
+
 var app = builder.Build();
+
+app.MapOpenApi();                   // Apply Open API middleware to pipeline
 
 app.MapGet("/", () => "Filter Pipeline Demo");
 
